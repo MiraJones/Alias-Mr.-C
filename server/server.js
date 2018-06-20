d5492a27a561281c99ca58b659b16e63e4b3f2bc
Mira Jones <meschoolstuffya@gmail.com>
2018-06-20 12:33:17 -0600

    Server

--- server/server.js
+++ server/server.js
@@ -5,4 +5,4 @@ var app = express();
 app.use(bodyParser.json());
 app.use( express.static( `${__dirname}/../build` ) );
 
-app.listen(3030, console.log("Zombies to fight on port 3030"))
+app.listen(3031, console.log("Zombies to fight on port 3031"))
