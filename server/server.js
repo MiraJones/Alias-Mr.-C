1f0c85d8b2b74da02635c99896b0c359f92dc116
Mira Jones <meschoolstuffya@gmail.com>
2018-06-20 12:27:55 -0600

    Fixed Server

--- server/server.js
+++ server/server.js
@@ -3,6 +3,6 @@ var bodyParser = require("body-parser");
 
 var app = express();
 app.use(bodyParser.json());
-
+app.use( express.static( `${__dirname}/../build` ) );
 
 app.listen(3030, console.log("Zombies to fight on port 3030"))
